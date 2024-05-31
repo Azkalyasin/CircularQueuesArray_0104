56572ac8f4e59ca73af74690f3ee8dd7d2dd7a0b
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-31 10:37:27 +0700

    menambahkan isi function insert

--- StackAlgoritma/StackAlgoritma.cpp
+++ StackAlgoritma/StackAlgoritma.cpp
@@ -15,9 +15,31 @@ public:
 
 
 	void insert() {
-	
+		int num;
+		cout << "enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh
+		if ((front == 0 && rear == Max - 1) || (front == rear + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+		//cek antrian kosong
+		if (front == -1) {
+			front = 0;
+			rear = 0;
+		}
+		else {
+			//jika reare berada di akhir array
+			if (rear == Max - 1)
+				rear = 0;
+			else
+				rear = rear + 1;
+		}
 		queue_array[rear] = num;
 	}
+
 
 
 
